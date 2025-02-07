cd6b09400b37320a083eee0444073a12547ebb5e
Thalia Ruiz <thalia.ruizespitia@gmail.com>
2025-02-07 16:00:19 -0600

    Actualizando App

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let amigos = [];
+let ul = document.querySelector("#listaAmigos");
 
 function agregarAmigo(){
     let amigo = document.querySelector("#amigo").value;
@@ -12,7 +13,34 @@ function agregarAmigo(){
 
         amigos.push(amigo);
         document.querySelector("#amigo").value= "";
+        mostrarAmigo();
     }
 
 }
 
+function mostrarAmigo(){
+
+    ul.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(amigos[i]));
+        ul.appendChild(li);
+
+    }
+}
+
+function sortearAmigo(){
+
+    if(amigos == ""){
+        alert("Ingresa los nombres para poder iniciar el sorteo");
+    }else{
+        let sorteado = Math.floor(Math.random()*amigos.length);
+
+        ul.innerHTML = "";
+
+        let resultado = document.querySelector("#resultado");
+        resultado.innerHTML = "¡El amigo sorteado es: " + amigos[sorteado] + "!";
+    }
+}
