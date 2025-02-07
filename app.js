e20d041becce6528836013a11205933230305e45
Thalia Ruiz <thalia.ruizespitia@gmail.com>
2025-02-06 23:30:25 -0600

    Actualizando archivos

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo(){
+    let amigo = document.querySelector("#amigo").value;
+
+    if(amigo==""){
+        alert("Por favor, inserte un nombre");
+
+    }else {
+
+        amigos.push(amigo);
+        document.querySelector("#amigo").value= "";
+    }
+
+}
+
